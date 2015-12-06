429a25248a8d45bdf3b609f3c6435475e608a309
quezlatch <quezlatch@gmail.com>
2015-12-06 12:16:31 +0000

    problem 1 without modulo

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -3,10 +3,12 @@
 [<Tests>]
 let problems = testList "first few problems" [
                 testCase "1" <| fun _ ->
-                    let actual = [1..999] |> Seq.where (fun x -> x % 3 =0 || x % 5 = 0) |> Seq.sum
+                    let takeFirst1000 = Seq.takeWhile (fun x -> x < 1000)
+                    let multiplesOf3 = Seq.initInfinite ( fun i -> i * 3) |> takeFirst1000
+                    let multiplesOf5 = Seq.initInfinite ( fun i -> i * 5) |> takeFirst1000
+                    let actual =  Seq.append multiplesOf3 multiplesOf5 |> Seq.distinct|> Seq.sum
                     Assert.Equal("", 233168, actual)
      ]
 
 [<EntryPoint>]
 let main args = defaultMainThisAssembly args
-
