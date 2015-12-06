0b0a1496458f185176dba96f6661fe583e7d8277
quezlatch <quezlatch@gmail.com>
2015-12-06 14:47:43 +0000

    problem 2. fibonacci generator

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -7,6 +7,17 @@ let problems = testList "first few problems" [
                     let multiplesOf5 = [0 .. 5 .. 999]
                     let actual =  Seq.append multiplesOf3 multiplesOf5 |> Seq.distinct|> Seq.sum
                     Assert.Equal("", 233168, actual)
+
+                testCase "2" <| fun _ ->
+                    let fibonacci =
+                        let rec s i j = seq {
+                                yield j
+                                yield! s j (i + j)
+                            }
+                        s 1 1
+                    let actual = fibonacci |> Seq.take 6 |> Seq.toList
+                    Assert.Equal("",[1; 2; 3; 5; 8; 13], actual)
+                
      ]
 
 [<EntryPoint>]
