b81c8ecc440e8fe046ef2d56b2afc4e15fac3915
quezlatch <quezlatch@gmail.com>
2015-12-06 12:05:18 +0000

    initial problem 1 solution

---
+++ ProjectEuler/Program.fs
@@ -0,0 +1,12 @@
+﻿open Fuchu
+
+[<Tests>]
+let problems = testList "first few problems" [
+                testCase "1" <| fun _ ->
+                    let actual = [1..999] |> Seq.where (fun x -> x % 3 =0 || x % 5 = 0) |> Seq.sum
+                    Assert.Equal("", 233168, actual)
+     ]
+
+[<EntryPoint>]
+let main args = defaultMainThisAssembly args
+
