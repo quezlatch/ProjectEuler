b29da774abb3de93f4529aa83e274d0fc60586cf
quezlatch <quezlatch@gmail.com>
2015-12-06 20:22:25 +0000

    generate even fibonacci numbers straight off the bat

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -9,13 +9,14 @@ let problems = testList "first few problems" [
                     Assert.Equal("", 233168, actual)
 
                 testCase "2" <| fun _ ->
-                    let fibonacci =
+                    // x,y -> y, x + y -> x + y, x + 2y -> x + 2y, 2x + 3y
+                    let evenFibonacci =
                         let rec s i j = seq {
                                 yield j
-                                yield! s j (i + j)
+                                yield! s (i + 2*j) (2*i + 3*j)
                             }
-                        s 1 1
-                    let actual = fibonacci |> Seq.where (fun i -> i &&& 1 = 0) |> Seq.takeWhile (fun i -> i < 4000000) |> Seq.sum
+                        s 1 2
+                    let actual = evenFibonacci |> Seq.takeWhile (fun i -> i < 4000000) |> Seq.sum
                     Assert.Equal("", 4613732, actual)
                 
      ]
