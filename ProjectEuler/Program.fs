9320a0eb26b56fe7296c4c5a67f214033f020754
quezlatch <quezlatch@gmail.com>
2015-12-06 12:32:40 +0000

    problem 1 with list comprehension

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -3,9 +3,8 @@
 [<Tests>]
 let problems = testList "first few problems" [
                 testCase "1" <| fun _ ->
-                    let takeFirst1000 = Seq.takeWhile (fun x -> x < 1000)
-                    let multiplesOf3 = Seq.initInfinite ( fun i -> i * 3) |> takeFirst1000
-                    let multiplesOf5 = Seq.initInfinite ( fun i -> i * 5) |> takeFirst1000
+                    let multiplesOf3 = [0 .. 3 .. 999]
+                    let multiplesOf5 = [0 .. 5 .. 999]
                     let actual =  Seq.append multiplesOf3 multiplesOf5 |> Seq.distinct|> Seq.sum
                     Assert.Equal("", 233168, actual)
      ]
